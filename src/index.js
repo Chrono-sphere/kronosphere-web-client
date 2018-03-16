3d65c7634f79e7ef99a40c7ba938d7ca57e34abf
Vishrut Reddi <vishrutreddi@gmail.com>
2018-03-16 00:41:10 -0700

    Add a blank template with extra setup (#4) (#5)
    resolves #1

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { createHttpLink  } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+import { HashRouter } from 'react-router-dom';
+
+import App from './components/App';
+
+const link = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+  credentials: 'include'
+});
+
+const client = new ApolloClient({
+    dataIdFromObject: o => o.id,
+    link,
+    cache: new InMemoryCache(),
+});
+
+const Root = () => {
+    return (
+        <ApolloProvider client={ client }>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </ApolloProvider>
+    );
+}
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
